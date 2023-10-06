0729257516629ba5d78dc43a65e725a5d8724ccd
DavidFlutter <david.haastrup@stu.cu.edu.ng>
2023-10-06 19:07:30 +0000

    Header and hero done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import heroImage from "./assets/images/image-hero.jpg"
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="wrapper">
+        <img src={heroImage} alt="" />
+        <Header></Header>
+        <Hero></Hero>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'>
+        <h1>loopstudio</h1>
+        <nav>
+            <span>About</span>
+            <span>Contact</span>
+            <span>Products</span>
+            <span>Support</span>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
